7934f0673a2c95a727cf9bd63c2e9f7d39facce4
bijayakc <bkc8@uco.edu>
2026-06-08 22:47:12 -0500

    i cloned my file with new project

--- src/main/java/com/bijayakc/demo/HelloController.java
+++ src/main/java/com/bijayakc/demo/HelloController.java
@@ -3,7 +3,14 @@ package com.bijayakc.demo;
 
 public class HelloController {
 
-    public String home(){
+    public String home() {
+
+        String[] cars = new String[3];
+        cars[0] = "volvo";
+        cars[1] = "BMW";
+        cars[2] = "ford";
+        System.out.println(cars[2]);
+        System.out.println(cars.length);
         return " lets make a controller";
     }
 }
